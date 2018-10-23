0f0bfca5ba34507b6ac230023aef54c119cc3ab5
Stacy Kim <Stacy@Stacys-MacBook-Air-2.local>
2018-10-23 15:16:04 -0400

    NPM scripts working and folders created

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  // This is the 'root' of our client-side application + where webpack will start bundling things up
+  entry: `${SRC_DIR}/index.jsx`,
+  // This is where the index.html lives and where the bundle.js will go
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        // File extension we are looking for
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
