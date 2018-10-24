e2fb2696f5e3a0f55251826c567108489abef743
Stacy Kim <Stacy@Stacys-MacBook-Air-2.local>
2018-10-24 11:14:58 -0400

    Database schema created

--- database/index.js
+++ database/index.js
@@ -1,8 +1,30 @@
+/* REMEMBER TO DELETE DROP DATABSE FROM SCHEMA.SQL WHEN EVERYTHING IS WORKING */
+
 const mysql = require('mysql');
 const config = require('../config.js');
+// var connection = mysql.createConnection(config);
+// connection.connect((err) => {
+//   if (err) { return console.error('error connecting: ' + err.stack); }
+//   else { console.log('connected as id ' + connection.threadId); }
+// });
 
-var db = mysql.createConnection(config);
-db.connect((err) => {
-  if (err) { return console.error('error connecting: ' + err.stack); }
-  else { console.log('connected as id ' + connection.threadId); }
+const knex = require('knex')({ client: 'mysql', connection: config });
+const bookshelf = require('bookshelf')(knex);
+
+var Listing = bookshelf.Model.extend({
+  tableName: 'Listings'
 });
+
+var User = bookshelf.Model.extend({
+  tableName: 'Users'
+});
+
+var Booking = bookshelf.Model.extend({
+  tableName: 'Bookings'
+});
+
+var Review = bookshelf.Model.extend({
+  tableName: 'Reviews'
+});
+
+module.exports = connection;
---
+++ database/schema.sql
@@ -0,0 +1,43 @@
+DROP DATABASE staybnb;
+
+CREATE DATABASE staybnb;
+USE staybnb;
+
+CREATE TABLE Listings (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE Users (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(50),
+  `display_name` VARCHAR(50),
+  `photo_url` VARCHAR(200),
+  `profile_url` VARCHAR(200),
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE Bookings (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `listings_id` INTEGER NOT NULL,
+  `users_id` INTEGER NOT NULL,
+  `stay_start` DATE,
+  `stay_end` DATE,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`listings_id`) REFERENCES Listings(`id`),
+  FOREIGN KEY (`users_id`) REFERENCES Users(`id`)
+);
+
+CREATE TABLE Reviews (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `bookings_id` INTEGER NOT NULL,
+  `review_date` DATE,
+  `accuracy` TINYINT CHECK (`accuracy` < 6),
+  `communication` TINYINT,
+  `cleanliness` TINYINT,
+  `location` TINYINT,
+  `check-in` TINYINT,
+  `value` TINYINT,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`bookings_id`) REFERENCES Bookings(`id`)
+);
