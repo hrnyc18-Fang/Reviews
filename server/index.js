a55cf90fa48436bab089770c7cb9f82e817540b4
Stacy Kim <Stacy@Stacys-MacBook-Air-20645.local>
2018-11-01 16:31:51 -0400

    CORS headers

--- server/index.js
+++ server/index.js
@@ -9,16 +9,16 @@ const router = require('./router.js');
 const app = exprgess();
 const source = path.join(__dirname, '/../client/dist');
 
+app.all('/*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+ });
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(source));
 
 app.use('/', router);
-
-app.all('/*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
- });
 
 app.listen(7000, () => { console.log('Listening on port 7000'); });
 
