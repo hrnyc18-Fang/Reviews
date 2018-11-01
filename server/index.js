a42ed89b27b1dc60decf690fa24d71cb32704848
Stacy Kim <Stacy@Stacys-MacBook-Air-20645.local>
2018-11-01 16:45:23 -0400

    Fixed typo

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ const cors = require('cors');
 const db = require('../database/index.js');
 const router = require('./router.js');
 
-const app = exprgess();
+const app = express();
 const source = path.join(__dirname, '/../client/dist');
 
 app.use(bodyParser.json());
 
 app.use(cors());
-app.all('/', function(req, res, next) {
+app.all('/*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
